1e73301965f262b982b3e982384d56711f493665
Nicole D <nnardella@gmail.com>
2019-01-02 15:03:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create
-
+  def self.create(name: " ", grade: " ")
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
 
